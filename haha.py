099f736ec72c54b4838eb2625de79c259551ad7f
Tr00y2ooo <08005252a@gmail.com>
2022-01-13 01:16:17 +0800

    20220113

---
+++ haha.py
@@ -0,0 +1,3 @@
+d = "0x123"
+a = int(d.split[0],16)
+print(a)
---
+++ web.py
@@ -0,0 +1,7 @@
+import webbrowser
+import requests
+urlstr = "https://www.twse.com.tw/"
+webbrowser.open(urlstr)
+
+urlstr = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=csv&date=20210712&stockNo=2330"
+webbrowser.open(urlstr)
